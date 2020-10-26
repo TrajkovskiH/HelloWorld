e28193086bb9301a8fa12891ee82c9bab5d5e3d3
Hristijan Trajkovski <trajkovski.hristijan@uklo.edu.mk>
2020-10-26 14:19:01 +0100

    Dodaden link do Github reposirtory vo komentar

--- src/com/trajkovski/hristijan/Main.java
+++ src/com/trajkovski/hristijan/Main.java
@@ -3,5 +3,6 @@ package com.trajkovski.hristijan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HelloWorld OOP FIKT");
-    } //testasdfgasdfgsdfghsdfg
+    }
+    //Github repository: https://github.com/TrajkovskiH/HelloWorld.git
 }
